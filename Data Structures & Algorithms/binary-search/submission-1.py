9eeddf566eabf6386c25d18ae1357e9cd8241567
Amogh Sundararaman <83564560+amoghsundararaman@users.noreply.github.com>
2026-05-28 08:21:41 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, hi = 0, len(nums) - 1
+
+        while low <= hi: 
+            mid = low + (hi - low) // 2
+
+            if nums[mid] == target: 
+                return mid
+            elif nums[mid] > target:
+                hi = mid - 1
+            else: 
+                low = mid + 1
+        return -1
+        
